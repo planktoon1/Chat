398d4b4a6269d6c861a3cc6605a29d2f54acf3da
Frederik Østergaard <fkoe90@gmail.com>
2020-09-25 16:47:43 +0200

    AddMembersToGroupChat function implemented

--- backend/src/chatFunctions.ts
+++ backend/src/chatFunctions.ts
@@ -6,9 +6,63 @@ AWS.config.update({ region: "eu-central-1" });
 const ddb = new AWS.DynamoDB.DocumentClient();
 const tableName = "Chats"; //TODO: Dynamic table name
 export const markMessagesAsRead = async (params) => {};
-export const addGroupMembers = async (params) => {};
 export const removeGroupMembers = async (params) => {};
 export const setChatMembersState = async (params) => {};
+
+interface AddMembersToGroupChatInput {
+  chatId: string;
+  memberIds: string[];
+  adminIds: string[];
+}
+/**
+ * Add members to a group chat.
+ * @param {Object} params - The input values for the function.
+ */
+export const addMembersToGroupChat = async (
+  params: AddMembersToGroupChatInput
+) => {
+  const now = new Date().toISOString();
+  const memberPutRequests = params.memberIds.map((memberId) => ({
+    PutRequest: {
+      Item: {
+        CreatedAt: now,
+        ChatId: params.chatId,
+        SortKey: `member_${memberId}`,
+        GSI1PK: `member_${memberId}`,
+        GSI1SK: now,
+      },
+    },
+  }));
+
+  const adminPutRequests = params.adminIds.map((adminId) => ({
+    PutRequest: {
+      Item: {
+        CreatedAt: now,
+        ChatId: params.chatId,
+        State: `admin`,
+        SortKey: `member_${adminId}`,
+        GSI1PK: `member_${adminId}`,
+        GSI1SK: now,
+      },
+    },
+  }));
+
+  try {
+    const response = await ddb
+      .batchWrite({
+        RequestItems: {
+          [tableName]: [...memberPutRequests, ...adminPutRequests],
+        },
+      })
+      .promise();
+    console.info(
+      `Successfully added "${params.memberIds}" to group: "${params.chatId}"`
+    );
+    return response;
+  } catch (error) {
+    handleCommonErrors(error);
+  }
+};
 
 interface BaseGetInput {
   limit?: number;
--- backend/src/test.ts
+++ backend/src/test.ts
@@ -1,17 +1,22 @@
 import AWS from "aws-sdk";
-import { getChatsForUser, getMessagesInChat } from "./chatFunctions";
+import { addMembersToGroupChat } from "./chatFunctions";
 AWS.config.update({ region: "eu-central-1" });
 
 const app = async () => {
-  let res: any = { lastEvaluated: undefined };
+  addMembersToGroupChat({
+    adminIds: ["newAdmin1"],
+    chatId: "GC_921a0f1f-9e0e-4768-a836-fdbf79534b41",
+    memberIds: ["newMember1", "newMember2"],
+  });
 
-  do {
-    res = await getChatsForUser({
-      userId: "larsman1",
-      limit: 3,
-      lastEvaluated: res.lastEvaluated,
-    });
-    console.log(res);
-  } while (res.lastEvaluated);
+  // let res: any = { lastEvaluated: undefined };
+  // do {
+  //   res = await getChatsForUser({
+  //     userId: "larsman1",
+  //     limit: 3,
+  //     lastEvaluated: res.lastEvaluated,
+  //   });
+  //   console.log(res);
+  // } while (res.lastEvaluated);
 };
 app();
