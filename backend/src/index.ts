33e5b0c720d6a5905d4998f043bdce3ef88d5032
Frederik Østergaard <fkoe90@gmail.com>
2020-09-30 21:43:29 +0200

    Testing websockets with graphql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,51 +1,130 @@
-import { ApolloServer } from "apollo-server-lambda";
 import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLNonNull,
-  GraphQLString,
-} from "graphql";
+  DynamoDBEventProcessor,
+  DynamoDBConnectionManager,
+  DynamoDBEventStore,
+  DynamoDBSubscriptionManager,
+  PubSub,
+  Server,
+  withFilter,
+} from "aws-lambda-graphql";
+import { ApiGatewayManagementApi, DynamoDB } from "aws-sdk";
+import * as assert from "assert";
+import { ulid } from "ulid";
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "RootQueryType", // an arbitrary name
-    fields: {
-      // the query has a field called 'greeting'
-      greeting: {
-        // we need to know the user's name to greet them
-        args: { firstName: { type: new GraphQLNonNull(GraphQLString) } },
-        // the greeting message is a string
-        type: GraphQLString,
-        // resolve to a greeting message
-        resolve: (parent, args) => "Greeting resolver",
-      },
+// serverless offline support
+const dynamoDbClient = new DynamoDB.DocumentClient({
+  // use serverless-dynamodb endpoint in offline mode
+  ...(process.env.IS_OFFLINE
+    ? {
+        endpoint: "http://localhost:8000",
+      }
+    : {}),
+});
+
+const eventStore = new DynamoDBEventStore({ dynamoDbClient });
+const pubSub = new PubSub({ eventStore });
+const subscriptionManager = new DynamoDBSubscriptionManager({ dynamoDbClient });
+const connectionManager = new DynamoDBConnectionManager({
+  // this one is weird but we don't care because you'll use it only if you want to use serverless-offline
+  // why is it like that? because we are extracting api gateway endpoint from received events
+  // but serverless offline has wrong stage and domainName values in event provided to websocket handler
+  // so we need to override the endpoint manually
+  // please do not use it otherwise because we need correct endpoint, if you use it similarly as dynamoDBClient above
+  // you'll end up with errors
+  apiGatewayManager: process.env.IS_OFFLINE
+    ? new ApiGatewayManagementApi({
+        endpoint: "http://localhost:3001",
+      })
+    : undefined,
+  dynamoDbClient,
+  subscriptions: subscriptionManager,
+});
+
+type MessageType = "greeting" | "test";
+
+type Message = {
+  id: string;
+  text: string;
+  type: MessageType;
+};
+
+type SendMessageArgs = {
+  text: string;
+  type: MessageType;
+};
+
+const typeDefs = /* GraphQL */ `
+  enum MessageType {
+    greeting
+    test
+  }
+  type Message {
+    id: ID!
+    text: String!
+    type: MessageType!
+  }
+  type Mutation {
+    sendMessage(text: String!, type: MessageType = greeting): Message!
+  }
+  type Query {
+    serverTime: Float!
+  }
+  type Subscription {
+    messageFeed(type: MessageType): Message!
+  }
+`;
+
+const resolvers = {
+  Mutation: {
+    async sendMessage(rootValue: any, { text, type }: SendMessageArgs) {
+      assert.ok(text.length > 0 && text.length < 100);
+      const payload: Message = { id: ulid(), text, type };
+
+      await pubSub.publish("NEW_MESSAGE", payload);
+
+      return payload;
     },
-  }),
-  mutation: new GraphQLObjectType({
-    name: "RootMutationType", // an arbitrary name
-    fields: {
-      changeNickname: {
-        args: {
-          firstName: { type: new GraphQLNonNull(GraphQLString) },
-          nickname: { type: new GraphQLNonNull(GraphQLString) },
+  },
+  Query: {
+    serverTime: () => Date.now(),
+  },
+  Subscription: {
+    messageFeed: {
+      resolve: (rootValue: Message) => {
+        // root value is the payload from sendMessage mutation
+        return rootValue;
+      },
+      subscribe: withFilter(
+        pubSub.subscribe("NEW_MESSAGE"),
+        (rootValue: Message, args: { type: null | MessageType }) => {
+          // this can be async too :)
+          if (args.type == null) {
+            return true;
+          }
+
+          return args.type === rootValue.type;
+        }
+      ),
+    },
+  },
+};
+
+const server = new Server({
+  connectionManager,
+  eventProcessor: new DynamoDBEventProcessor(),
+  resolvers,
+  subscriptionManager,
+  // use serverless-offline endpoint in offline mode
+  ...(process.env.IS_OFFLINE
+    ? {
+        playground: {
+          subscriptionEndpoint: "ws://localhost:3001",
         },
-        type: GraphQLString,
-        resolve: (parent, args) => "Change nickname resolver",
-      },
-    },
-  }),
+      }
+    : {}),
+  typeDefs,
 });
 
-const server = new ApolloServer({
-  schema,
-
-  // By default, the GraphQL Playground interface and GraphQL introspection
-  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
-  //
-  // If you'd like to have GraphQL Playground and introspection enabled in production,
-  // the `playground` and `introspection` options must be set explicitly to `true`.
-  playground: true,
-  introspection: true,
-});
-
-exports.graphqlHandler = server.createHandler();
+export const handleHttp = server.createHttpHandler();
+export const handleWebSocket = server.createWebSocketHandler();
+export const handleDynamoDBStream = server.createEventHandler();
